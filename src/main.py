fe7b32a36e00bd6965a779ac791d3e1cc021d048
pfg-data-data <117166798+pfg-data-data@users.noreply.github.com>
2023-03-19 18:22:19 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,35 @@
+# Google sheet
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# -----------------------------
 from prefect import flow, task
 from prefect.blocks.system import JSON
 
 json_block = JSON.load('pfg-sheet-credentials')
 
-import json
-d = json.loads(str(json_block))
+
+# Connect to Google Sheets
+sheet_list_url          = '1c-fmeZbQGs2jESqCH9lZlm80cH2CR9VEHvWNwFbsjeo'
+
+scope = ['https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive"]
+
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(json_block, scope)
+gc = gspread.authorize(credentials)
+
+
+
 
 @task(name ="step 1")
 def step1():
     print('dfd')
 
+    
+    
+    
+    
+    
 @flow(name="Do stupid thing")
 def print_something():
     step1()
