93346fff587946926135c44ae0e1a0271bab2e92
pfg-data-data <117166798+pfg-data-data@users.noreply.github.com>
2023-03-20 14:43:05 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -14,22 +14,35 @@ from prefect.blocks.system import JSON
 json_block = JSON.load("pfg-sheet-credentials")
 a= json_block.value
 
+sheet_list_url          = '1c-fmeZbQGs2jESqCH9lZlm80cH2CR9VEHvWNwFbsjeo'
+
+scope = ['https://www.googleapis.com/auth/spreadsheets',
+     "https://www.googleapis.com/auth/drive"]
+
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(a, scope)
+gc = gspread.authorize(credentials)
+
+# Create sheet function
+def pd_extract_sheet_data(destination_database_url, destination_sheet):
+    # open sheet and extract all data
+    wks = gc.open_by_key(destination_database_url).worksheet(destination_sheet)
+    data = wks.get_all_records()
+    return data
+
+
+
 @task(name ="step 1")
 def step1():
     print('dfd')
+    
+@task(name ="step 2")
+def step1():
+    pd_extract_sheet_data(sheet_list_url, 'test')
 
     
 @flow(name="Do stupid thing")
 def print_something():
     step1()
+    b = pd_extract_sheet_data()
+    print(b)
 
-
-    print(a)
-    print(type(a))
-    sheet_list_url          = '1c-fmeZbQGs2jESqCH9lZlm80cH2CR9VEHvWNwFbsjeo'
-
-    scope = ['https://www.googleapis.com/auth/spreadsheets',
-         "https://www.googleapis.com/auth/drive"]
-
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict(a, scope)
-    gc = gspread.authorize(credentials)
